c3b5afb68c3eefbe538284890df1aa8123c8bfcd
Nkepers <nick.kepers.coder@gmail.com>
2021-09-18 20:05:09 -0700

    created a few start js lines.

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+//Player will be prompted to create a name for their robot.
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This is a string , good for leaving myself messages.");
+//this one will do math
+console.log(10 + 10);
+//This will tell us our robots name
+console.log("Your robot's name is" + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+// fight();
